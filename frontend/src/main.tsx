4613ab6a99307c29ebf132469b1ae308d3b2db0e
Anwesha <anwesha.birati@gmail.com>
2026-07-05 22:23:55 +0530

    fixxxx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,19 @@
-import posthog from "posthog-js";
-
-posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
-  api_host: import.meta.env.VITE_POSTHOG_HOST,
-  capture_pageview: false, // handled by PageTracker
-  autocapture: true,
-  loaded: (ph) => {
-    if (import.meta.env.DEV) ph.debug();
-  }
-});
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import posthog from "posthog-js";
 import { App } from "./App";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
 import "./styles.css";
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  capture_pageview: false,
+  autocapture: true,
+  loaded: (ph) => {
+    if (import.meta.env.DEV) ph.debug();
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
